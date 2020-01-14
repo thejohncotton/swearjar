d6acb0b1c340f5ad21e1b567838ec89285617556
John Cotton <john@1080people.com>
2020-01-14 18:52:13 -0500

    Initial Commit

---
+++ swearjar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swearjar
+//
+//  Created by John Cotton on 1/14/20.
+//  Copyright © 2020 John Cotton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
